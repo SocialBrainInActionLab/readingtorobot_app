1d01ccddc43aeec5263dad3349fa5f51faf4feee
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-04-02 15:21:58 +1100

    Added new message for control app
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- server/slack_bot.py
+++ server/slack_bot.py
@@ -16,5 +16,14 @@ def publish_hostname() -> None:
                                       text="Access Reading To Robot App in: http://{}:5000".format(host))
 
 
+def publish_control_hostname() -> None:
+    host = os.popen("hostname -I | awk '{print $1;}'").read()[0:-1]
+    token = os.environ.get('SLACK_TOKEN')
+    if token is not None:
+        slack_client = WebClient(token)
+        slack_client.chat_postMessage(channel="reading_to_robot",
+                                      text="Access Control App in: http://{}:5001".format(host))
+
+
 if __name__ == '__main__':
     publish_hostname()
