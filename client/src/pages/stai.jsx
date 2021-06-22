d8d3445573a0a720814a43b056c4a8c14cea517e
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-06-22 15:11:22 +1000

    Updated STAI page.

--- client/src/pages/stai.jsx
+++ client/src/pages/stai.jsx
@@ -1,116 +1,73 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import styled from 'styled-components';
-import Brightness1Icon from '@material-ui/icons/Brightness1';
-import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Grid, Box, Divider } from '@material-ui/core';
+import { Page, IntensityButtons } from '../components';
 
-import { DragDropContext } from 'react-beautiful-dnd';
-import DragArea, { Box } from '../components/dnd';
-
-const Image = styled.img`
-  max-width: 60%;
-`;
-
-const initData = {
-  cards: {
-    calm: { id: 'calm', content: <Image src={`${process.env.PUBLIC_URL}/calm_face.gif`} alt="calm" /> },
-    happy: { id: 'happy', content: <Image src={`${process.env.PUBLIC_URL}/happy_face.gif`} alt="happy" /> },
-    sad: { id: 'sad', content: <Image src={`${process.env.PUBLIC_URL}/sad_face.gif`} alt="sad" /> },
-    upset: { id: 'upset', content: <Image src={`${process.env.PUBLIC_URL}/upset_face.gif`} alt="upset" /> },
-  },
-  fields: {
-    origin: {
-      id: 'origin',
-      title: '',
-      cardIds: ['calm', 'happy', 'sad', 'upset'],
-    },
-    low: {
-      id: 'low',
-      title: '',
-      cardIds: [],
-    },
-    mid: {
-      id: 'mid',
-      title: '',
-      cardIds: [],
-    },
-    high: {
-      id: 'high',
-      title: '',
-      cardIds: [],
-    },
-  },
-  fieldOrder: ['low', 'mid', 'high'],
-};
-
-export default class STAI extends DragArea {
-  constructor(props) {
-    super(props);
-    this.state = initData;
-  }
-
+export default class STAI extends Page {
   render() {
-    const { fieldOrder, cards } = this.state;
-    const fields = this.getFields();
+    const { qId } = this.props;
 
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Grid container direction="column" alignItems="center">
-
-          <Grid
-            container
-            direction="column"
-            justify="space-between"
-            alignItems="stretch"
-            style={{ width: '80vw' }}
-          >
-            <Grid item>
-              <Grid
-                container
-                justify="space-around"
-                alignItems="center"
-              >
-                <Grid item xs={4}>
-                  <Brightness1Icon style={{ fontSize: 30, color: 'red' }} />
-                </Grid>
-                <Grid item xs={4}>
-                  <Brightness1Icon style={{ fontSize: 70, color: 'red' }} />
-                </Grid>
-                <Grid item xs={4}>
-                  <Brightness1Icon style={{ fontSize: 140, color: 'red' }} />
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid
-                container
-                justify="space-around"
-                alignItems="stretch"
-              >
-                {fieldOrder.map((fieldId) => {
-                  const field = fields[fieldId];
-                  const cardIds = field.cardIds.map((cardId) => cards[cardId]);
-
-                  return (
-                    <Grid item xs={4}>
-                      <Box key={field.id} box={field} cards={cardIds} bordered />
-                    </Grid>
-                  );
-                })}
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Box
-                width="100%"
-                direction="horizontal"
-                key={fields.origin.id}
-                box={fields.origin}
-                cards={fields.origin.cardIds.map((cardId) => cards[cardId])}
-              />
-            </Grid>
+      <Box m={5}>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <p>How much do you agree with each of these statements?</p>
           </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I feel calm"
+              qId={`${qId}_calm`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I am tense"
+              qId={`${qId}_tense`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I feel upset"
+              qId={`${qId}_upset`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I feel relaxed"
+              qId={`${qId}_relaxed`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I feel content"
+              qId={`${qId}_content`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
+          <Grid item>
+            <Box height="20px" />
+            <IntensityButtons
+              question="I feel worried"
+              qId={`${qId}_worried`}
+            />
+          </Grid>
+          <Grid item><Divider /></Grid>
         </Grid>
-      </DragDropContext>
+      </Box>
     );
   }
 }
+
+STAI.propTypes = {
+  qId: PropTypes.string.isRequired,
+};
--- server/run.py
+++ server/run.py
@@ -27,9 +27,10 @@ csv_fieldnames = ['id', 'date', 'name', 'phone', 'email', 'birthdate', 'age', 'g
                   'miro_q1', 'miro_q2', 'miro_q3', 'nao_q1', 'nao_q2', 'nao_q3', 'cozmo_q1', 'cozmo_q2', 'cozmo_q3',
                   'chosen', 'q0', 'q1_option', 'q1_extended', 'q2', 'q3', 'q4', 'q5', 'q6_extended', 'q6_option',
                   'q7_extended', 'q7_option', 'q8_extended', 'q8_option' 'q9', 'q10', 'q11', 'q12', 'q13', 'q14',
-                  'rating1_first', 'rating1_origin', 'rating1_second', 'rating1_third', 'rating2_first',
-                  'rating2_origin', 'rating2_second', 'rating2_third', 'stai1_high', 'stai1_low', 'stai1_mid',
-                  'stai1_origin', 'stai2_high', 'stai2_low', 'stai2_mid', 'stai2_origin', 'videos']
+                  'videos', 'rating1_first', 'rating1_origin', 'rating1_second', 'rating1_third', 'rating2_first',
+                  'rating2_origin', 'rating2_second', 'rating2_third', 'stai1_calm', 'stai1_content', 'stai1_relaxed',
+                  'stai1_tense', 'stai1_upset', 'stai1_worried', 'stai2_calm', 'stai2_content', 'stai2_relaxed',
+                  'stai2_tense', 'stai2_upset', 'stai2_worried']
 running_robot = '_'
 robot_ips = {}
 data_file = '/data/data.csv'
